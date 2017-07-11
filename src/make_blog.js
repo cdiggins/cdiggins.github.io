408124e4d195e6e1917fd023f47642953f87e8c8
Christopher Diggins <cdiggins@gmail.com>
2017-07-10 22:48:55 -0400

    It works!

--- src/make_blog.js
+++ src/make_blog.js
@@ -1,9 +1,26 @@
 'use strict';
 
-let fs = require("fs");
-let path = require('path');
-let mdToHtml = require('myna-parser/tools/myna_markdown_to_html');
-let expand = require('myna-parser/tools/myna_mustache_expander');
+var fs = require("fs");
+var path = require('path');
+var myna = require('myna-parser');
+var mdAstToHtml = require('../src/markdown_to_html');
+var mdGrammar = require('myna-parser/grammars/grammar_markdown')(myna);
+var expand = require('myna-parser/tools/myna_mustache_expander');
+
+/*
+    TODO: 
+    * create the index page
+    * create the blog list 
+    * make the dates shorter 
+    * fix the prev/next links 
+    * make the prev/next links of the article conditional 
+    * don't repeat the first line of the article twice. 
+    * extract a short description from the article ? 
+    * add some hover text for the different links (particular blog articles)
+
+    LOWPRI:
+    * favicon
+*/
 
 function replaceExt(fileName, newExt) {
     let i = fileName.lastIndexOf(".");
@@ -15,8 +32,26 @@ function createLink(href, text) {
     return "<a href='" + href + "'>" + text + "</a>";
 }
 
+function articleUrl(article) {
+    return article ? article.url : "";
+}
+
 function articleLink(article) {
     return article ? createLink(article.url, article.title) : '';
+}
+
+function prevNextNav(article) {
+    var prev = article.prev;
+    var next = article.next; 
+    var r = "";
+    if (prev) {
+        r += createLink(prev.url, "<< " + prev.title);
+        if (next) r += " | ";
+    }
+    if (next) {
+        r += createLink(next.url, next.title + " >>");
+    }
+    return r;
 }
 
 function saveToFile(folder, fileName, content) {
@@ -27,7 +62,14 @@ function readJsonFile(file) {
     return JSON.parse(fs.readFileSync(file, 'utf-8'));
 }
 
-function generateBlogs(inputFolder, outputFolder, templateFile, articlesFile) {
+// Outputs HTML from markdown given a string 
+function mdToHtml(input) {
+    let rule = myna.allRules['markdown.document'];
+    let ast = myna.parse(rule, input);
+    return mdAstToHtml(ast, []).join("");
+}
+
+function generateBlogArticles(inputFolder, outputFolder, templateFile, articlesFile) {
     if (!fs.existsSync(outputFolder))
         fs.mkdirSync(outputFolder);
     
@@ -44,37 +86,50 @@ function generateBlogs(inputFolder, outputFolder, templateFile, articlesFile) {
     articles.forEach(function(a) { a.date = new Date(a.date); });
 
     // Sort articles by date    
-    articles.sort(function (a,b) { return b.date -a.date; });
+    articles.sort(function (a,b) { return b.date - a.date; });
 
     // Generate the content for each page 
     for (let i=0; i < articles.length; ++i) {
-        try {
-            let a = articles[i];
-            a.url = replaceExt(a.src, ".html");
-            a.prev = articles[i+1];
-            a.next = articles[i-1];
-            a.linkPrev = articleLink(a.prev);
-            a.linkNext = articleLink(a.next);
-            a.sideBar = '';
-            a.srcFile = inputFolder + '/' + a.src;
-            a.markDown = fs.readFileSync(a.srcFile, 'utf-8');
-            a.content = mdToHtml(a.markDown);
-            a.html = expand(template, a);
-            saveToFile(outputFolder, a.url, a.html);
-        }
-        catch (ex)
-        {
-            console.log(ex);
-        }
-
+        let a = articles[i];
+        a.date = a.date.toLocaleDateString('en-us', { weekday:'long', year:'numeric', month:'long', day:'numeric'});
+        a.url = replaceExt(a.src, ".html");
+        a.prev = articles[i+1];
+        a.next = articles[i-1];
     }
+
+    for (var a of articles) {
+        a.urlPrev = articleUrl(a.prev);
+        a.urlNext = articleUrl(a.next);
+        a.linkPrev = articleLink(a.prev);
+        a.linkNext = articleLink(a.next);
+        a.prevNextNav = prevNextNav(a);
+        a.sideBar = '';
+        a.srcFile = inputFolder + '/' + a.src;
+        a.markDown = fs.readFileSync(a.srcFile, 'utf-8');
+        a.content = mdToHtml(a.markDown);
+        a.html = expand(template, a);
+        saveToFile(outputFolder, a.url, a.html);
+    }
+
+    return articles;
 }
 
-generateBlogs(
-    './src/articles', 
-    './blog/', 
-    './src/article_template.html', 
-    './src/articles.json');
+function main() {
+    var articles = generateBlogArticles(
+        './src/articles', 
+        './blog/', 
+        './src/article_template.html', 
+        './src/articles.json');
+        
+    var blog_template = fs.readFileSync('./blog_template.html', 'utf-8');
+    var blog_html = expand(blog_template, { articles:articles });
+    saveToFile('./', 'blog.html', blog_html);
 
+    var index_template = fs.readFileSync('./index_template.html', 'utf-8');
+    var index_html = expand(index_template, { articles:articles });
+    saveToFile('./', 'index.html', index_html);
+}
+
+main();
 console.log('finished');
 process.exit();
